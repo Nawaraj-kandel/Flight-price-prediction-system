1dfb6140cadda48255e52c0b7ef7c8ce91738bef
bhuwan <ricky.pantha@gmail.com>
2025-02-18 22:09:30 +0545

    fix: token param access

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -74,17 +74,18 @@
 // export default ResetPassword;
 
 import { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { API_BASE_URL } from "../Api/Api";
 
 const ResetPassword = () => {
-  const { token } = useParams();
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get('token');
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
